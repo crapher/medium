ffc4b17688e232da29bf87c3d727f319af45fdba
Diego Degese <ddegeselists@gmail.com>
2023-05-03 09:10:43 -0500

    Reinforcement learning environment to train and predict the next close price

---
+++ 03.ReinforcementLearning/next_close_env.py
@@ -0,0 +1,74 @@
+import numpy as np
+import gym
+from gym import spaces
+
+OBS_MIN_MAX = 0.05
+
+class NextCloseEnv(gym.Env):
+
+    def __init__(self, observation_size, closes):
+
+        # Data
+        self.__features = closes[:-1]
+        self.__targets = closes[1:]
+
+        # Spaces
+        self.observation_space = spaces.Box(low=np.NINF, high=np.PINF, shape=(observation_size,), dtype=np.float32)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
+
+        # Episode Management
+        self.__start_tick = observation_size
+        self.__end_tick = len(self.__targets)
+        self.__current_tick = self.__end_tick
+
+    def reset(self):
+
+        # Reset the current tick pointer and return a new observation
+        self.__current_tick = self.__start_tick
+        return self.__get_observation()
+
+    def step(self, action):
+
+        # If current tick is over the last index in the feature array, the environment needs to be reset
+        if self.__current_tick > self.__end_tick:
+            raise Exception('The environment needs to be reset.')
+
+        # Compute the step reward (Penalize the agent with the absolute difference between the real value and the prediction)
+        step_reward = -abs(action[0] - self.__current_target)
+
+        # Generate the custom info array with the real and predicted values
+        info = {
+            'agent_target': action[0],
+            'calculated_target': self.__current_target,
+            'agent_real_target': (action[0] * OBS_MIN_MAX + 1) * self.__current_avg,
+            'real_target': self.__real_target}
+
+        # Increase the current tick pointer, check if the environment is fully processed, and get a new observation
+        self.__current_tick += 1
+        done = self.__current_tick >= self.__end_tick
+        obs = self.__get_observation()
+
+        # Returns the observation, the step reward, the status of the environment, and the custom information
+        return obs, step_reward, done, info
+
+    def __get_observation(self):
+
+        # If current tick over the last value in the feature array, the environment needs to be reset
+        if self.__current_tick >= self.__end_tick:
+            return None
+
+        # Save the real observation and target values
+        self.__real_observation = self.__features[(self.__current_tick - self.__start_tick):self.__current_tick]
+        self.__real_target = self.__targets[self.__current_tick]
+
+        # Generate a copy of the observation and target to avoid changing the original data
+        self.__current_observation = self.__real_observation.copy()
+        self.__current_target = self.__real_target.copy()
+
+        # Calculate values between -1 and 1 for the new observation without leak any data
+        self.__current_avg = np.mean(self.__current_observation)
+        self.__current_observation = np.clip((self.__current_observation / self.__current_avg - 1) / OBS_MIN_MAX, -1, 1)
+        self.__current_target = np.clip((self.__current_target / self.__current_avg - 1) / OBS_MIN_MAX, -1, 1)
+
+        # Return the calculated observation
+        return self.__current_observation
---
+++ 03.ReinforcementLearning/run.py
@@ -0,0 +1,60 @@
+import math
+import numpy as np
+import pandas as pd
+
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+from next_close_env import NextCloseEnv
+
+# Read the data and generate the train and test dataframes
+df = pd.read_csv('OIH_15T.csv.gz', compression='gzip')
+train = df[df['date'] <= '2022-01-01']
+test = df[df['date'] > '2022-01-01']
+
+# Create 4 parallel train environments
+env = make_vec_env(NextCloseEnv, seed=42, n_envs=4, env_kwargs={'observation_size': 26, 'closes': train['close'].values})
+
+# Train the model
+model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=100000000)
+
+# Save, remove, and reload the model (To be sure it works as expected)
+model.save("ppo_next_close")
+del model
+model = PPO.load("ppo_next_close")
+
+# Create a test environmant
+env = NextCloseEnv(observation_size=26, closes=test['close'].values)
+
+# Create the required variables for calculation
+real = []
+predicted = []
+done = False
+
+# Predict the test values with the trained model
+obs = env.reset()
+while not done:
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+
+    # Save the values to calculate the errors
+    real.append(info['real_target'])
+    predicted.append(info['agent_real_target'])
+
+    print(f"AT: {info['agent_target']:6.3f} - CT: {info['calculated_target']:6.3f} - ART: {info['agent_real_target']:9.3f} - RT: {info['real_target']:9.3f}")
+
+# Show results
+real = np.array(real)
+predicted = np.array(predicted)
+
+mse = np.square(real - predicted).mean()
+rmse = math.sqrt(mse)
+mae = np.mean(np.abs(real - predicted))
+mape = np.mean(np.abs((real - predicted) / real)) * 100
+
+print(' RESULT '.center(56, '*'))
+print(f"* Mean Square Error:              {mse:.3f}")
+print(f"* Root Mean Square Error:         {rmse:.3f}")
+print(f"* Mean Absolute Error:            {mae:.3f}")
+print(f"* Mean Absolute Percentage Error: {mape:.3f} %")
